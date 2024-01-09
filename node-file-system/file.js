e8643120433ef675009b32eccb30510769e22974
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-10 00:27:55 +0400

    async file read and write

--- node-file-system/file.js
+++ node-file-system/file.js
@@ -1,4 +1,16 @@
 const fs = require('fs')
 
+// read a file
 const text = fs.readFileSync('./text/input.txt','utf-8')
 console.log(text)
+
+// write to a file
+
+const textToWrite = `I am writing via FS \n thanks on ${Date.now()}`
+fs.writeFileSync('./text/input.txt', textToWrite)
+
+//Async file
+
+fs.readFile('./text/start.txt','utf-8', (err,data) => {
+    console.log(data)
+})
